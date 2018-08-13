e7f4567d445108331ccbaf1bf0a76a54a919f129
Nilton J F <>
2018-08-13 11:13:14 -0300

    database script

--- web/src/test/MainTest.java
+++ web/src/test/MainTest.java
@@ -1,16 +1,23 @@
 package test;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class MainTest {
 
 	public static void main(String[] args) {
 
-		
-//		Date dt = new Date();
-//		Calendar c = new Calendar("01/06/2018");
-		
+		String dtFromDB = "01/06/2018 21:35:43";
+		StringBuilder date = new StringBuilder(dtFromDB);
+
+		String[] dateArray = dtFromDB.split(" ");
+//		dateArray[0] = new StringBuilder((String) dateArray[0]).reverse().toString().split("/");
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Calendar calendar = new GregorianCalendar(2013, 1, 28, 13, 24, 56);
+
 	}
 
 }
